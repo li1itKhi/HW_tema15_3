2f9206112d05b952c88a142bf2451940dbc0b548
Лилит <@LAPTOP-3EJS7JBS>
2023-05-28 02:09:54 +0300

    Добавьте файлы проекта.

---
+++ HW_tema15_3/Program.cs
@@ -0,0 +1,26 @@
+﻿int[] array = new int[10]; //инициализация массива
+
+for(int i=0; i<array.Length; i++) //заполнение массива с клавиатуры
+{
+    Console.WriteLine("Введите элемент массива: ");
+
+    array[i] = int.Parse(Console.ReadLine()); //вводим элементы
+}
+
+for(int i=0; i < array.Length; i++) //вывод элементов массива
+{
+    Console.Write(array[i]+" ");
+}
+Console.WriteLine(); //разделение
+
+Console.Write("Введите число, которое хотите найти: ");
+int number=int.Parse(Console.ReadLine()); //переменная для ввода числа, которое будем искать в массиве
+
+Array.Sort(array); //сортировка массива через link
+
+for (int i=0; i < array.Length; i++)
+{
+    Console.Write(array[i]+" "); //вывод отсортированного массива
+}
+//                        определяем индекс элемента в массиве
+Console.WriteLine("Позиция: "+Array.IndexOf(array, number));
